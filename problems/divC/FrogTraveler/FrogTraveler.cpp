ef0b190764e3b6211fb1f4c8d6b77dbdb1be5d0e
JRazek <jakub.jakubecky123@gmail.com>
2021-10-27 00:10:24 +0200

    almost

--- problems/divC/FrogTraveler/FrogTraveler.cpp
+++ problems/divC/FrogTraveler/FrogTraveler.cpp
@@ -5,49 +5,27 @@ using namespace std;
 
 #define INF 3*1e7
 
-int getJumps(int lvl, vector<int> &dp, vector<int> &dpStates, vector<int> &jumps, vector<int> &slips, vector<int> &path){
-    auto lvlOk = [](int lvl, int n){
-        return (lvl >= 0 && lvl <= n);
-    };
-    if(!lvlOk(lvl, dp.size()-1))
-        return INF;
-    if(lvl == 0){
-        dpStates[0] = 1;
-        dp[0] = 0;
-    }
-    if(dpStates[lvl] == 1)
+int getJumps(int lvl, vector<int> &dp, vector<int> &jumps, vector<int> &slips, vector<int> &path, set<int> &notRested){
+    if(notRested.find(lvl) == notRested.end())
         return dp[lvl];
-    else if(dpStates[lvl] == -1)
-        return INF;
-
-    dpStates[lvl] = -1;
+    notRested.erase(lvl);
+    if(lvl == 0){
+        dp[lvl] = 0;
+        return dp[lvl];
+    }
     pair<int, int> best = {INF, -1};
-    for(int i = 0; i <= jumps[lvl]; i ++){
-        int nextLvl = lvl - i;
-        if(lvl == 9 && nextLvl == 4)
-            cout<<"";
-        if(lvl == 2 && nextLvl == 0)
-            cout<<"";
-        if(lvlOk(nextLvl, dp.size()-1)){
-            int tmp = nextLvl;
-            nextLvl += slips[nextLvl];
-            int val = getJumps(nextLvl, dp, dpStates, jumps, slips, path) + 1;
+
+    //start from the node that is satisfied the first condition and end on the last that satisfied the last
+    for(auto nr : notRested){
+        if(lvl > nr && nr >= lvl - jumps[lvl]){
+            int val = getJumps(nr + slips[nr], dp, jumps, slips, path, notRested) + 1;
             if(val < best.first){
-                if(lvl == 4)
-                    cout<<"";
-                best = {val, nextLvl};
-                path[lvl] = tmp;
+                best = {val, nr};
             }
         }
     }
-    if(best.second != -1){
-        dpStates[lvl] = 1;
-        dp[lvl] = best.first;
-    }else{
-        dpStates[lvl] = -1;
-        dp[lvl] = INF;
-    }
-    
+    dp[lvl] = best.first;
+    path[lvl] = best.second;
     return dp[lvl];
 }
 void getPath(int lvl, vector<int> &path, vector<int> &slips, list<int> &str){
@@ -60,11 +38,14 @@ void getPath(int lvl, vector<int> &path, vector<int> &slips, list<int> &str){
 int main(){
     int n;
     cin >> n;
-    vector<int> dp(n+1, 0);
-    vector<int> dpStates(n+1, 0);
+    vector<int> dp(n+1, INF);
     vector<int> jumps(n+1, 0);
     vector<int> slips(n+1, 0);
     vector<int> path(n+1, 0);
+    set<int> notRested;
+    for(int i = 0; i <= n; i ++){
+        notRested.insert(i);
+    }
     for(int i = 0; i < n;  i++){
         int num;
         cin >> num;
@@ -75,8 +56,8 @@ int main(){
         cin >> num;
         slips[i+1] = num;
     }
-    getJumps(n, dp, dpStates, jumps, slips, path);
-    bool ok = dpStates[n] == 1;
+    getJumps(n, dp, jumps, slips, path, notRested);
+    bool ok = dp[n] < INF;
     cout<<(ok ? dp[n] : -1)<<"\n";
     if(ok){
         list<int> str;
@@ -85,7 +66,5 @@ int main(){
             cout<<s<<" ";
         }
     }
-    //frog traveler O(N^2 time).
-    //to optimize!
     return 0;
 }
