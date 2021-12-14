767bbc26d21a26b4fa8b05ca712303b005716dbc
JRazek <jakub.jakubecky123@gmail.com>
2021-12-14 02:21:37 +0100

    sgtree...

--- dataStructures/SegmentTree.h
+++ dataStructures/SegmentTree.h
@@ -1,17 +1,20 @@
 #include <vector>
 #include <cmath>
 #include <cstdint>
+#include <list>
+#include <iostream>
 
 namespace sgNs{
     enum class SGMODE{
+        SUM,
+        XOR,
         MIN,
         MAX,
-        SUM,
     };
     template<
-        SGMODE Mode,
-        typename Y,
+        SGMODE Mode = SGMODE::SUM,
         typename T = u_int64_t,
+        typename Y = void*,
         typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type
     >
     class SegmentTree{
@@ -20,25 +23,71 @@ namespace sgNs{
             T numericVal;
             Y data;
         };
+        struct Range{
+            u_int low;
+            u_int high;
+            Range common(Range rhs) const{
+                return {std::max(high, rhs.high), std::min(low, rhs.low)};
+            }
+        };
     private:
-        std::vector<Node> data;
+        std::vector<Node> nodes;
+        std::vector<Range> ranges;
         u_int realSize;
-    public:
-        SegmentTree(const std::vector<Node>& _data){
-            realSize = std::pow(2, (std::ceil(std::log2(_data.size())) + 1)) - 1;
-            u_int height = std::ceil(std::log2(data.size()))+1;
-            int firstFloorID = std::pow(2, height - 1) - 1;
-            data = std::vector<Node>(realSize - _data.size());
-            std::copy(_data.begin(), _data.end(), data.begin());
+
+        void preprocess(){
+            u_int fdID=firstFloorID();
+            for(int i = fdID; i<nodes.size(); i++){
+                ranges[i].low=i-fdID;
+                ranges[i].high=i-fdID;
+            }
+            for(int i = firstFloorID()-1; i>=0; i--){
+                ranges[i] = {ranges[childID(i, true)].low, ranges[childID(i, false)].high};
+                if constexpr(Mode == SGMODE::SUM){
+                    nodes[i].numericVal = nodes[childID(i, true)].numericVal+nodes[childID(i, false)].numericVal;
+                }else if constexpr(Mode == SGMODE::MIN){
+                    
+                }else if constexpr(Mode == SGMODE::MAX){
+
+                }
+            }
+            std::cout<<"";
+        }
+        std::list<u_int> rangeQuery(Range r, u_int parent) const{
+            if(r.low > r.high)
+                return {};
+            r=r.common(ranges[parent]);
+            if(ranges[parent]==r){
+                return {parent};
+            }
+            std::list<u_int> left = rangeQuery(r, childID(true, parent));
+            std::list<u_int> right = rangeQuery(r, childID(false, parent));
+            // return std::copy() [todo] join these two in O(1)
         }
 
-        u_int getChildID(u_int parentID, bool left){
+    public:
+        SegmentTree(const std::vector<Node>& _data):
+        nodes(std::pow(2, (std::ceil(std::log2(_data.size())) + 1)) - 1),
+        ranges(nodes.size()),
+        realSize(_data.size())
+        {
+            std::copy(_data.begin(), _data.end(), nodes.begin()+firstFloorID());
+            preprocess();
+        }
+        u_int height() const{
+            return std::log2(nodes.size()+1);
+        }
+        u_int firstFloorID() const{
+            return std::pow(2, height() - 1) - 1;
+        }
+        u_int childID(u_int parentID, bool left) const{
             return (parentID + 1)*2 + !left - 1;
         }
-        u_int getParentID(u_int childID){
+        u_int getParentID(u_int childID) const{
             return (childID - 1)/2;
         }
-        std::vector<Node> rangeQuery(uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1) const{
+
+        T rangeQuery(Range r){
             if constexpr(Mode == SGMODE::SUM){
                 
             }else if constexpr(Mode == SGMODE::MIN){
--- dataStructures/main.cpp
+++ dataStructures/main.cpp
@@ -1,9 +1,9 @@
 #include "SegmentTree.h"
 
 int main(){
-    using Sgtree = sgNs::SegmentTree<sgNs::SGMODE::SUM, void*, int>;
+    using Sgtree = sgNs::SegmentTree<sgNs::SGMODE::SUM, int>;
     using Node = Sgtree::Node;
     Node node;
-    Sgtree segmentTree({node});
+    Sgtree segmentTree({node, node, node, node});
     return 0;
 }
