e05695daf82fc444db3546891ba171e7645c5c09
JRazek <jakub.jakubecky123@gmail.com>
2021-12-14 15:20:21 +0100

    range query

--- dataStructures/SegmentTree.h
+++ dataStructures/SegmentTree.h
@@ -27,22 +27,19 @@ namespace sgNs{
             u_int low;
             u_int high;
             Range common(Range rhs) const{
-                return {std::max(high, rhs.high), std::min(low, rhs.low)};
+                return {std::max(low, rhs.low), std::min(high, rhs.high)};
             }
+            bool operator==(const Range& r){
+                return low==r.low&&high==r.high;
+            };
         };
     private:
         std::vector<Node> nodes;
-        std::vector<Range> ranges;
         u_int realSize;
 
         void preprocess(){
             u_int fdID=firstFloorID();
-            for(int i = fdID; i<nodes.size(); i++){
-                ranges[i].low=i-fdID;
-                ranges[i].high=i-fdID;
-            }
             for(int i = firstFloorID()-1; i>=0; i--){
-                ranges[i] = {ranges[childID(i, true)].low, ranges[childID(i, false)].high};
                 if constexpr(Mode == SGMODE::SUM){
                     nodes[i].numericVal = nodes[childID(i, true)].numericVal+nodes[childID(i, false)].numericVal;
                 }else if constexpr(Mode == SGMODE::MIN){
@@ -56,19 +53,22 @@ namespace sgNs{
         std::list<u_int> rangeQuery(Range r, u_int parent) const{
             if(r.low > r.high)
                 return {};
-            r=r.common(ranges[parent]);
-            if(ranges[parent]==r){
+            Range nr=nodeRange(parent);
+            r=r.common(nr);
+            if(nr==r){
                 return {parent};
             }
-            std::list<u_int> left = rangeQuery(r, childID(true, parent));
-            std::list<u_int> right = rangeQuery(r, childID(false, parent));
-            // return std::copy() [todo] join these two in O(1)
+            std::list<u_int> left = rangeQuery(r, childID(parent, true));
+            std::list<u_int> right = rangeQuery(r, childID(parent, false));
+
+            left.insert(left.end(), right.begin(), right.end());
+
+            return left;
         }
 
     public:
         SegmentTree(const std::vector<Node>& _data):
         nodes(std::pow(2, (std::ceil(std::log2(_data.size())) + 1)) - 1),
-        ranges(nodes.size()),
         realSize(_data.size())
         {
             std::copy(_data.begin(), _data.end(), nodes.begin()+firstFloorID());
@@ -77,8 +77,11 @@ namespace sgNs{
         u_int height() const{
             return std::log2(nodes.size()+1);
         }
+        u_int height(u_int nodeID) const{
+            return std::log2(nodeID+1);
+        }
         u_int firstFloorID() const{
-            return std::pow(2, height() - 1) - 1;
+            return std::pow(2, height()-1)-1;
         }
         u_int childID(u_int parentID, bool left) const{
             return (parentID + 1)*2 + !left - 1;
@@ -86,8 +89,16 @@ namespace sgNs{
         u_int getParentID(u_int childID) const{
             return (childID - 1)/2;
         }
-
+        Range nodeRange(u_int nodeID) const{
+            u_int nh=height(nodeID);
+            u_int h=height();
+            u_int sz=std::pow(2, h-nh-1);
+            u_int shift=(nodeID-((u_int)std::pow(2, nh)-1))*std::pow(2, h-nh-1);
+            return {shift, shift+sz-1};
+        }
         T rangeQuery(Range r){
+            std::list<u_int> q=rangeQuery(r, 0);
+
             if constexpr(Mode == SGMODE::SUM){
                 
             }else if constexpr(Mode == SGMODE::MIN){
@@ -95,6 +106,7 @@ namespace sgNs{
             }else if constexpr(Mode == SGMODE::MAX){
 
             }
+            return T();
         }
     };
 }
