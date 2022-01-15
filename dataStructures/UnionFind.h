a6470c661a735d0f6a8ed3ffb728af451ea136c2
JRazek <jakub.jakubecky123@gmail.com>
2022-01-15 15:17:09 +0100

    fix

--- dataStructures/UnionFind.h
+++ dataStructures/UnionFind.h
@@ -3,19 +3,20 @@
 
 template<typename T>
 struct UnionNode{
-	UnionNode* parent=this;
-	T data=T();
-
+	UnionNode* parent;
+	T data;
+	UnionNode(): parent(this), data(T()){}
 	UnionNode* getRoot(){
-		UnionNode* root=(parent==this ? this : &parent->getRoot());
+		UnionNode* root=(parent==this ? this : parent->getRoot());
 		parent=root;
 		return root;
 	}
 	static void joinUnion(UnionNode* n1, UnionNode* n2){
-		UnionNode* r1=n1.getRoot();
-		UnionNode* r2=n2.getRoot();
+		UnionNode* r1=n1->getRoot();
+		UnionNode* r2=n2->getRoot();
 		r1=r2->parent;
 	}
 };
+
 
 #endif //UNION_FIND_H
