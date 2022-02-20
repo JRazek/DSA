9c2696b6c09ba1f3801ba613990f658cca272a9b
JRazek <jakub.jakubecky123@gmail.com>
2022-02-20 18:00:51 +0100

    Update UnionFind.h

--- dataStructures/UnionFind.h
+++ dataStructures/UnionFind.h
@@ -1,20 +1,11 @@
-#ifndef UNION_FIND_H
-#define UNION_FIND_H
-
-template<typename T>
 struct UnionNode{
 	UnionNode* parent;
-	T data;
-	UnionNode(): parent(this), data(T()){}
-	UnionNode* getRoot(){
-		parent=(parent==this ? this : parent->getRoot());
-		return parent;
+	UnionNode(): parent(this){}
+	UnionNode& getRoot(){
+		return *(parent=(parent==this ? this : &parent->getRoot()));
 	}
-	void joinUnion(UnionNode* n1){
-		UnionNode* r1=n1->getRoot();
-		r1->parent=getRoot();
+	void joinUnion(UnionNode& n1){
+		auto& r1=n1.getRoot();
+		r1.parent=&getRoot();
 	}
 };
-
-
-#endif //UNION_FIND_H
