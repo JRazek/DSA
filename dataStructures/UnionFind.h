b363a411409fafdac0b2b25aa53e41ecf9e08dd4
JRazek <jakub.jakubecky123@gmail.com>
2022-07-31 09:25:28 +0200

    Update UnionFind.h

--- dataStructures/UnionFind.h
+++ dataStructures/UnionFind.h
@@ -1,11 +1,19 @@
+
 struct UnionNode{
 	UnionNode* parent;
 	UnionNode(): parent(this){}
+
 	UnionNode& getRoot(){
 		return *(parent=(parent==this ? this : &parent->getRoot()));
 	}
 	void joinUnion(UnionNode& n1){
 		auto& r1=n1.getRoot();
 		r1.parent=&getRoot();
+	}
+
+	static std::size_t count(std::ranges::range auto& range){
+		std::set<UnionNode*> unique;
+		for(auto& i : range) unique.insert(&i.getRoot());
+		return unique.size();
 	}
 };
